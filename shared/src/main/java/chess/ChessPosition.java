c1c83c7ed116662db8f62ba330689556ed7b7e88
Spencer4792 <spencerhales4792@gmail.com>
2024-07-02 10:56:23 -0600

    Added framework to ChessPosition.java.

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,29 +1,45 @@
 package chess;
 
-/**
- * Represents a single square position on a chess board
- * <p>
- * Note: You can add to this class, but you may not alter
- * signature of the existing methods.
- */
 public class ChessPosition {
+    private final int row;
+    private final int col;
 
     public ChessPosition(int row, int col) {
-    }
+            this.row = row;
+            this.col = col;
+        }
 
-    /**
-     * @return which row this position is in
-     * 1 codes for the bottom row
-     */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return row;
     }
 
-    /**
-     * @return which column this position is in
-     * 1 codes for the left row
-     */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return col;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ChessPosition that = (ChessPosition) o;
+
+        if (row != that.row) return false;
+        return col == that.col;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = row;
+        result = 31 * result + col;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "ChessPosition{" +
+                "row=" + row +
+                ", col=" + col +
+                '}';
     }
 }
