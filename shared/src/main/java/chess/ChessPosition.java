4b67199a621c8533b636b0e673461e32e4d09072
Spencer4792 <spencerhales4792@gmail.com>
2024-07-02 13:54:55 -0600

    Added chesspiece.javaframework and condensed chessposition.java.

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,13 +1,19 @@
 package chess;
 
+/**
+ * Represents a single square position on a chess board
+ * <p>
+ * Note: You can add to this class, but you may not alter
+ * signature of the existing methods.
+ */
 public class ChessPosition {
     private final int row;
     private final int col;
 
     public ChessPosition(int row, int col) {
-            this.row = row;
-            this.col = col;
-        }
+        this.row = row;
+        this.col = col;
+    }
 
     public int getRow() {
         return row;
@@ -21,18 +27,13 @@ public class ChessPosition {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-
         ChessPosition that = (ChessPosition) o;
-
-        if (row != that.row) return false;
-        return col == that.col;
+        return row == that.row && col == that.col;
     }
 
     @Override
     public int hashCode() {
-        int result = row;
-        result = 31 * result + col;
-        return result;
+        return 31 * row + col;
     }
 
     @Override
