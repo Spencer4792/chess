74d14a3c93a7063886d9fd947fa57d78d9548374
Spencer4792 <spencerhales4792@gmail.com>
2024-07-04 16:31:07 -0600

    Added chess piece classes and framework to chesspiece.java.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -3,13 +3,19 @@ package chess;
 import java.util.ArrayList;
 import java.util.Collection;
 
+/**
+ * Represents a single chess piece
+ * <p>
+ * Note: You can add to this class, but you may not alter
+ * signature of the existing methods.
+ */
 public class ChessPiece {
     private final ChessGame.TeamColor pieceColor;
     private final PieceType type;
 
     public ChessPiece(ChessGame.TeamColor pieceColor, PieceType type) {
-        this.pieceColor=pieceColor;
-        this.type=type;
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     public enum PieceType {
@@ -21,43 +27,74 @@ public class ChessPiece {
         PAWN
     }
 
-    /**
-     * @return Which team this chess piece belongs to
-     */
     public ChessGame.TeamColor getTeamColor() {
         return pieceColor;
     }
 
-    /**
-     * @return which type of chess piece this piece is
-     */
     public PieceType getPieceType() {
         return type;
     }
 
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        Collection<ChessMove> moves=new ArrayList<>();
-        // Implement the logic to calculate valid moves for the piece
-        // This will vary based on the type of piece
+        Collection<ChessMove> moves = new ArrayList<>();
+        switch (type) {
+            case KING:
+                addKingMoves(board, myPosition, moves);
+                break;
+            case QUEEN:
+                addQueenMoves(board, myPosition, moves);
+                break;
+            case BISHOP:
+                addBishopMoves(board, myPosition, moves);
+                break;
+            case KNIGHT:
+                addKnightMoves(board, myPosition, moves);
+                break;
+            case ROOK:
+                addRookMoves(board, myPosition, moves);
+                break;
+            case PAWN:
+                addPawnMoves(board, myPosition, moves);
+                break;
+        }
         return moves;
+    }
+
+    private void addKingMoves(ChessBoard board, ChessPosition myPosition, Collection<ChessMove> moves) {
+        // Implement king moves logic
+    }
+
+    private void addQueenMoves(ChessBoard board, ChessPosition myPosition, Collection<ChessMove> moves) {
+        // Implement queen moves logic
+    }
+
+    private void addBishopMoves(ChessBoard board, ChessPosition myPosition, Collection<ChessMove> moves) {
+        // Implement bishop moves logic
+    }
+
+    private void addKnightMoves(ChessBoard board, ChessPosition myPosition, Collection<ChessMove> moves) {
+        // Implement knight moves logic
+    }
+
+    private void addRookMoves(ChessBoard board, ChessPosition myPosition, Collection<ChessMove> moves) {
+        // Implement rook moves logic
+    }
+
+    private void addPawnMoves(ChessBoard board, ChessPosition myPosition, Collection<ChessMove> moves) {
+        // Implement pawn moves logic
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-
-        ChessPiece that=(ChessPiece) o;
-
-        if (pieceColor != that.pieceColor) return false;
-        return type == that.type;
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
     }
 
     @Override
     public int hashCode() {
-        int result=pieceColor != null ? pieceColor.hashCode() : 0;
-        result=31 * result + (type != null ? type.hashCode() : 0);
-        return result;
+        return 31 * pieceColor.hashCode() + type.hashCode();
     }
 
     @Override
