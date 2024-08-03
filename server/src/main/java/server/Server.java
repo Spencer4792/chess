ac337b59d591b3733581966d78f163dbb5397fa9
Spencer4792 <spencerhales4792@gmail.com>
2024-08-02 20:21:35 -0600

    Added authorization check to server.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -109,17 +109,16 @@ public class Server {
 
     private Object listGames(Request req, Response res) {
         try {
-            var authToken = req.headers("Authorization");
-            if (authToken == null || authToken.isEmpty()) {
-                res.status(401);
-                return gson.toJson(new ErrorResult("Error: unauthorized"));
-            }
-            var games = gameService.listGames(authToken);
+            String authToken = req.headers("Authorization");
+            Collection<GameData> games = gameService.listGames(authToken);
             res.status(200);
             return gson.toJson(new ListGamesResult(games));
         } catch (DataAccessException e) {
             res.status(401);
             return gson.toJson(new ErrorResult("Error: unauthorized"));
+        } catch (Exception e) {
+            res.status(500);
+            return gson.toJson(new ErrorResult("Error: " + e.getMessage()));
         }
     }
 
